540b798e7c1391364b781679c55984f05389493d
jslama911 <112126560+jslama911@users.noreply.github.com>
2023-01-08 16:06:21 -0600

    Used an array to improve function.

--- script.js
+++ script.js
@@ -1,137 +1,77 @@
-let x = 0;
-let y = 0;
-    
-    
-    function addition(){
-        if (x == 0){
-        x = document.getElementById("result").value
-        document.getElementById("result").value = "";
-        op = '+';
-        }
-        else{
-            y = document.getElementById("result").value
-            if (op == '+'){
-                var z = parseFloat(x) + parseFloat(y);
-            }
-            else if (op == '-'){
-                var z = parseFloat(x) - parseFloat(y);
-            }
-            else if (op == '*'){
-                var z = parseFloat(x) * parseFloat(y);
-            }
-            else if (op == '/'){
-                var z = parseFloat(x) / parseFloat(y);
-            }
-            x = z
-            document.getElementById("result").value = "";
-            op = '+';
-        }
-    }
-
-    function subtraction(){
-        if (x == 0){
-            x = document.getElementById("result").value
-            document.getElementById("result").value = "";
-            op = '-';
-        }
-        else{
-            y = document.getElementById("result").value
-            if (op == '+'){
-                var z = parseFloat(x) + parseFloat(y);
-            }
-            else if (op == '-'){
-                var z = parseFloat(x) - parseFloat(y);
-            }
-            else if (op == '*'){
-                var z = parseFloat(x) * parseFloat(y);
-            }
-            else if (op == '/'){
-                var z = parseFloat(x) / parseFloat(y);
-            }
-            x = z
-            document.getElementById("result").value = "";
-            op = '-';
-        }
-    }
-
-    function multiply(){
-        if (x == 0){
-            x = document.getElementById("result").value
-            document.getElementById("result").value = "";
-            op = '*';
-        }
-        else{
-            y = document.getElementById("result").value
-            if (op == '+'){
-                var z = parseFloat(x) + parseFloat(y);
-            }
-            else if (op == '-'){
-                var z = parseFloat(x) - parseFloat(y);
-            }
-            else if (op == '*'){
-                var z = parseFloat(x) * parseFloat(y);
-            }
-            else if (op == '/'){
-                var z = parseFloat(x) / parseFloat(y);
-            }
-            x = z
-            document.getElementById("result").value = "";
-            op = '*';
-        }
-    }
-
-    function division(){
-        if (x == 0){
-            x = document.getElementById("result").value
-            document.getElementById("result").value = "";
-            op = '/';
-        }
-        else{
-            y = document.getElementById("result").value
-            if (op == '+'){
-                var z = parseFloat(x) + parseFloat(y);
-            }
-            else if (op == '-'){
-                var z = parseFloat(x) - parseFloat(y);
-            }
-            else if (op == '*'){
-                var z = parseFloat(x) * parseFloat(y);
-            }
-            else if (op == '/'){
-                var z = parseFloat(x) / parseFloat(y);
-            }
-            x = z
-            document.getElementById("result").value = "";
-            op = '/';
-        }
-    }
-
-
-
-    function calculate(){
-        y = document.getElementById("result").value
-        if (op == '+'){
-            var z = parseFloat(x) + parseFloat(y);
-        }
-        else if (op == '-'){
-            var z = parseFloat(x) - parseFloat(y);
-        }
-        else if (op == '*'){
-            var z = parseFloat(x) * parseFloat(y);
-        }
-        else if (op == '/'){
-            var z = parseFloat(x) / parseFloat(y);
-        }
-        document.getElementById("result").value = z;
-        x = 0;
-        y = 0;
-    }
-    
+let nums = [];
 
     function display(value){
         document.getElementById("result").value += value;
     }
-    
+
     function clearScreen(){
         document.getElementById("result").value = "";
     }
+
+    function addition(){
+        let x = document.getElementById("result").value;
+        if (x != ""){
+            nums.push(x);
+            nums.push('+');
+            document.getElementById("result").value = "";
+            console.log(nums)
+        }
+    }
+
+    function subtraction(){
+        let x = document.getElementById("result").value;
+        if (x != ""){
+        nums.push(x);
+        nums.push('-');
+        document.getElementById("result").value = "";
+        console.log(nums)
+        }
+    }
+
+    function multiply(){
+        let x = document.getElementById("result").value;
+        if (x != ""){
+        nums.push(x);
+        nums.push('*');
+        document.getElementById("result").value = "";
+        console.log(nums)
+        }
+    }
+
+    function division(){
+        let x = document.getElementById("result").value;
+        if (x != ""){
+        nums.push(x);
+        nums.push('/');
+        document.getElementById("result").value = "";
+        console.log(nums)
+        }
+    }
+
+    function calculate(){
+        let x = document.getElementById("result").value;
+        nums.push(x);
+        document.getElementById("result").value = "";
+        let result = 0;
+        for (let i = 0; i < nums.length; i++){
+            if (nums[i] == '+'){
+                result = parseFloat(nums[i-1]) + parseFloat(nums[i+1]);
+                nums[i+1] = result;
+            }
+            else if (nums[i] == '-'){
+                result = parseFloat(nums[i-1]) - parseFloat(nums[i+1]);
+                nums[i+1] = result;
+            }
+            else if (nums[i] == '*'){
+                result = parseFloat(nums[i-1]) * parseFloat(nums[i+1]);
+                nums[i+1] = result;
+            }
+            else if (nums[i] == '/'){
+                result = parseFloat(nums[i-1]) / parseFloat(nums[i+1]);
+                nums[i+1] = result;
+            }
+        }
+        document.getElementById("result").value = result;
+        nums = [];
+    }
+
